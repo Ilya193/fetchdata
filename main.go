2ffbec98defb02b41c0558c55d6b5793428982cf
ilyapc <ilya.komendantov@list.ru>
2022-05-20 21:38:27 +0300

    fetch data

--- main.go
+++ main.go
@@ -1,13 +1,26 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+type Post struct {
+	userId int
+	id     int
+	title  string
+	body   string
+}
+
+type PostsCloud struct {
+	Posts []Post
+}
+
 func main() {
-	resp, err := http.Get("https://randomuser.me/api")
+	resp, err := http.Get("https://jsonplaceholder.typicode.com/posts")
 	if err != nil {
 		log.Fatalln(err)
 	}
@@ -17,5 +30,14 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	log.Println(string(body))
+	fmt.Println(string(body))
+
+	var posts PostsCloud
+
+	err = json.Unmarshal(body, &posts)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(posts)
 }
